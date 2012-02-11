abd9397f57aef2ada3617d140418e6f3ee141260
daisy1754 <d-part@hotmail.co.jp>
2012-02-11 16:15:54 +0900

    find strange behaviour (my additional elements once show up and then diminish...)

--- EditTweetButton/editButton.js
+++ EditTweetButton/editButton.js
@@ -1,11 +1,44 @@
 var itemsContainerId = 'stream-items-id';
 
-
 var streamItems = document.getElementById(itemsContainerId);
 var divs = streamItems.getElementsByTagName('div');
 
+// TODO 
+// twitter delete children of 'tweet' after page loaded...?
+// my elements once show up and then diminish...
+
 for (var i = 0; i < divs.length; i++) {
 	if (divs[i].getAttribute('data-item-type') == 'tweet') {
-		divs[i].innerText = 'hoge';
+		var editButton = createEditButton();
+		divs[i].appendChild(editButton);
+		divs[i].editButton = editButton;
+		divs[i].addEventListener('mouseover',
+			function(event){
+				if (event.target.editButton)
+					event.target.editButton.style.display = 'block';
+				else
+					console.log(event.target.getAttribute('tweet'));},
+			false);
+		divs[i].addEventListener('mouseout',
+			function(event){
+				if (event.target.editButton)
+					event.target.editButton.style.display = 'none';},
+			false);
 	}
 }
+
+function createEditButton() {
+	var editButton = document.createElement('div');
+	editButton.innerText = 'Edit';
+	editButton.className = 'editButton';
+	editButton.addEventListener('click', showEditTweetWindow(), false);
+	return editButton;
+}
+
+/**
+ * 
+ */
+function showEditTweetWindow() {
+	
+}
+
